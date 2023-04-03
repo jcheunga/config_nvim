3d5f22d10149cbaff17376bcc870169da1b04b52
Jason Cheung <jasoncheunga@gmail.com>
2023-04-03 18:22:48 +0900

    Init

---
+++ after/plugin/whitespace.lua
@@ -0,0 +1,15 @@
+require('whitespace-nvim').setup({
+    -- configuration options and their defaults
+
+    -- `highlight` configures which highlight is used to display
+    -- trailing whitespace
+    highlight = 'DiffDelete',
+
+    -- `ignored_filetypes` configures which filetypes to ignore when
+    -- displaying trailing whitespace
+    ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+})
+
+-- remove trailing whitespace with a keybinding
+vim.keymap.set('n', '<Leader>t', require('whitespace-nvim').trim)
+
