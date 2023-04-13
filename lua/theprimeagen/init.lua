88a362dd0c59b31638236729f9940ebbf21307bb
Jason Cheung <jasoncheunga@gmail.com>
2023-04-13 14:02:08 +0900

    More settings

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -32,3 +32,6 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -27,6 +27,7 @@ return require('packer').startup(function(use)
       end
   })
 
+  use("nvim-tree/nvim-tree.lua")
 
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
   use("nvim-treesitter/playground")
